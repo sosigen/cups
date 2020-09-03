ab9c4b4d27fe9e8416c499a3df7fb968401e75b9
sosigen <56933255+sosigen@users.noreply.github.com>
2020-09-03 19:55:10 +0200

    nieanimowane ikonki

---
+++ index.js
@@ -0,0 +1,14 @@
+class CupPile{
+    constructor(cups){
+        this.cups = {};
+        this.cups.ok = cups
+    }
+    moveLeft(){
+        this.cups
+    }
+    moveRight(){
+
+    }
+
+}
+let readyCups = new CupPile(Array.from(document.querySelectorAll('.fa')))
