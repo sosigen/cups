d0eda8c564103fbca51e7c3caf987e04bb5ba3df
sosigen <56933255+sosigen@users.noreply.github.com>
2020-09-04 17:25:51 +0200

    logiczcny switch ikonek

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 class CupPile{
-    constructor(cups){
-        this.cups = {};
-        this.cups.ok = cups
+    constructor([...cups]){
+        this.cups = cups;
+        // this.cups.ok = cups[0];
+        // this.cups.wait = cups[1];
+        // this.cups.error = cups[2];
     }
-    moveLeft(){
-        this.cups
+    moveLeft = () => {
+        const swap = (id1, id2, list=this.cups) =>{
+            let temp = list[id1];
+            list[id1] = list[id2];
+            list[id2] = temp;
+        }
+        console.log(this.cups)
+        swap(0,2);
+        swap(0,1);
+        console.log(this.cups)
     }
     moveRight(){
 
