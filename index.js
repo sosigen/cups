962dea5c01a87ca0f4ec8dfffeeacc9511f52729
sosigen <56933255+sosigen@users.noreply.github.com>
2020-09-04 23:58:00 +0200

    działający slider

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class CupPile{
     constructor(cups){
-        this.cups = {};
-        this.cups.ok = cups
+        this.cups = cups
+        this.cupSize = 600;
+        this.counter = 0;
+        this.slides = document.querySelector('#slides');
+        window.addEventListener("keydown", e => { this.moveSlide(e) } );
+        window.addEventListener("transitionend", this.repeatSlides);
     }
-    moveLeft(){
-        this.cups
+    moveSlide = (event) => {
+        this.slides.style.transition = "transform 0.5s ease-in-out"
+        switch(event.key){
+            case "ArrowRight":
+                this.counter++;
+                this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`            
+                break;
+            case "ArrowLeft":
+                this.counter--;
+                this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
+                break;
+            default:
+                console.log('nie strzalka')
+                break;
+        }
     }
-    moveRight(){
-
+    repeatSlides = () => {
+        if(this.cups[this.counter].id === 'okClone'){
+            this.slides.style.transition = 'none'
+            this.counter = this.cups.length - this.counter;
+            this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
+        }
+        if(this.cups[this.counter].id === 'waitClone'){
+            this.slides.style.transition = 'none'
+            this.counter = this.cups.length - 2;
+            this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
+        }
     }
 
 }
