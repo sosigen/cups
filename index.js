5176b20484fbd1a2ba10c52ee776d09ce3545758
sosigen <56933255+sosigen@users.noreply.github.com>
2020-09-05 11:58:26 +0200

    zmiana tła podczas przechodzenia między opcjami

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 class CupPile{
-    constructor(cups){
-        this.cups = cups
-        this.cupSize = 600;
+    constructor(){
+        this.cups = document.querySelectorAll('.fa')
+        this.cupSize = this.cups[0].clientWidth;
         this.counter = 0;
         this.slides = document.querySelector('#slides');
+        this.running = false;
         window.addEventListener("keydown", e => { this.moveSlide(e) } );
         window.addEventListener("transitionend", this.repeatSlides);
+        this.backgrounds = {
+            error: '#EF476F',
+            wait: '#FFD166',
+            ok: '#06D6A0'
+        }
     }
     moveSlide = (event) => {
-        this.slides.style.transition = "transform 0.5s ease-in-out"
-        switch(event.key){
-            case "ArrowRight":
-                this.counter++;
-                this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`            
-                break;
-            case "ArrowLeft":
-                this.counter--;
-                this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
-                break;
-            default:
-                console.log('nie strzalka')
-                break;
+        if(!this.running){
+            this.running = true;
+            this.slides.style.transition = "transform 0.5s ease-in-out"
+            switch(event.key){
+                case "ArrowRight":
+                    this.counter++;            
+                    break;
+                case "ArrowLeft":
+                    this.counter--;
+                    break;
+            }
+            this.changeBack();
+            this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
         }
     }
     repeatSlides = () => {
+        this.running = false;
         if(this.cups[this.counter].id === 'okClone'){
             this.slides.style.transition = 'none'
             this.counter = this.cups.length - this.counter;
@@ -34,7 +41,16 @@ class CupPile{
             this.counter = this.cups.length - 2;
             this.slides.style.transform = `translateX(${(-this.cupSize * this.counter)}px)`
         }
+
+        
+    }
+    changeBack = () => {
+        let current = this.cups[this.counter].id;
+        if(current.includes('Clone')){
+            current = current.slice(0, -5)
+        }
+        document.body.style.background = this.backgrounds[current];
     }
 
 }
-let readyCups = new CupPile(Array.from(document.querySelectorAll('.fa')))
+const readyCups = new CupPile()
