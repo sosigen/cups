96ee5e98781bfdbb58725286d5c1e889eaf69e43
sosigen <56933255+sosigen@users.noreply.github.com>
2020-09-07 19:50:53 +0200

    pop-up dokonczony wizualnie

--- login.js
+++ login.js
@@ -1,1 +1,5 @@
 const popup = document.querySelector('.pop-up');
+const closeButton = document.querySelector('#closeButton')
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
